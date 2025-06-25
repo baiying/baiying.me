98b354abc5c34d9ea68360f62b2c1596ce66ffae
bai ying <cn.baiying@gmail.com>
2025-06-25 11:52:03 +0800

    update

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -6,7 +6,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
   title: '老白的日志',
-  tagline: '老白的日志',
+  tagline: 'Hello, World!',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
